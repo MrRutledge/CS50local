c43bbcfc03d2fa38728657d864cf48a6323bf547
MrRutledge <rahimrutledge@gmail.com>
2022-11-08 16:22:34 +0000

    Mario supercat

--- C/agree.c
+++ C/agree.c
@@ -4,5 +4,14 @@
 
 int main(void)
 {
+   char c = get_char("Do you agree ? ");
     
+    if (c=='y' || c== 'Y')
+        {
+            printf("Agreed\n");
+        }
+        else if (c=='n'|| c=='N')
+        {
+            printf("Not Agreed\n");
+        }
 }
---
+++ C/meow.c
@@ -0,0 +1,21 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+
+    //While loop
+    /**int counter = 3;
+    while (counter > 0)
+    {
+       printf("meow\n");
+        counter-1;
+        
+    }**/
+
+    //for loop 
+    for (int i = 0; i<3; i++ )
+    {
+        printf("meow\n");
+    }
+}
