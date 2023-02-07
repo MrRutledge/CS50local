ce56737379f4d30b53608f75aa27fd4bd5598da3
MrRutledge <rahimrutledge@gmail.com>
2023-02-07 15:33:40 +0000

    Mario

--- C/hello.c
+++ C/hello.c
@@ -6,7 +6,7 @@ int main(void)
     string first = get_string("What's your name?\n");
     string last  = get_string("what's your last name\n");
 
-    //print first name and last name h//
+    //print first name and last name //
     printf("hello,%s %s\n" , first, last);
 
 }
